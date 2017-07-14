17e04a294ccc1e24651a419e08835bad95be7f56
Xueyou Luo <xueyou.luo@aidigger.com>
2017-07-14 18:43:34 +0800

    add toy data

--- model/basic_s2s_model.py
+++ model/basic_s2s_model.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, division, print_function
 import tensorflow as tf
 from tensorflow.python.layers.core import Dense
 import os
-from util import get_optimizer, multi_rnn_cell, single_rnn_cell
+from utils.model_util import get_optimizer, multi_rnn_cell, single_rnn_cell, create_attention_mechanism
 from model.config import BasicConfig
 
 
@@ -32,48 +32,12 @@ class BasicS2SModel(object):
             self.setup_train()
             self.setup_summary()        
         else:
+            #self.setup_inference_decoder_layer()
             self.setup_beam_search()
         self.setup_saver()
 
-    def setup_summary(self):
-        self.summary_writer = tf.summary.FileWriter(self.config.checkpoint_dir, self.sess.graph)
-        tf.summary.scalar("train_loss", self.losses)
-        tf.summary.scalar("learning_rate", self.learning_rate)
-        self.summary_op = tf.summary.merge_all()
-
-    def setup_saver(self):
-        self.saver = tf.train.Saver(tf.global_variables())
-
-    def restore_model(self,epoch = None):
-        if epoch is None:
-            self.saver.restore(self.sess,tf.train.latest_checkpoint(self.config.checkpoint_dir))
-        else:
-            self.saver.restore(self.sess,self.config.checkpoint_dir+ "model.ckpt" + ("-%d"%epoch))
-    
-    def save_model(self,epoch=None):
-        if epoch is None:
-            self.saver.save(self.sess,self.config.checkpoint_dir + "model.ckpt",global_step=self.global_step)
-        else:
-            self.saver.save(self.sess,self.config.checkpoint_dir + "model.ckpt",global_step=epoch)
-        
-    def setup_input_placeholders(self):
-        # if we use beam search decoder, we need to specify the batch size and max source len
-        # this is a bug I think, and tf will fix this in the future
-        if self.config.mode == "inference":
-            self.batch_size = self.config.batch_size
-            self.source_tokens = tf.placeholder(
-                tf.int32, shape=[self.batch_size, self.config.max_source_len])
-            self.source_length = tf.placeholder(
-                tf.int32, shape=[self.batch_size, ])
-        else:
-            self.source_tokens = tf.placeholder(tf.int32, shape=[None, None])
-            self.source_length = tf.placeholder(tf.int32, shape=[None, ])
-            # using dynamic batch size
-            self.batch_size = tf.shape(self.source_tokens)[0]
-
-        if self.train_phase:
-            self.target_tokens = tf.placeholder(tf.int32, shape=[None, None])
-            self.target_length = tf.placeholder(tf.int32, shape=[None, ])
+    def init(self):
+        self.sess.run(tf.global_variables_initializer())
 
     def setup_train(self):
         if self.config.exponential_decay:
@@ -91,6 +55,66 @@ class BasicS2SModel(object):
         self.param_norm = tf.global_norm(params)
         self.updates = opt.apply_gradients(
             zip(clipped_gradients, params), global_step=self.global_step)
+            
+    def setup_summary(self):
+        self.summary_writer = tf.summary.FileWriter(self.config.checkpoint_dir, self.sess.graph)
+        tf.summary.scalar("train_loss", self.losses)
+        tf.summary.scalar("learning_rate", self.learning_rate)
+        self.summary_op = tf.summary.merge_all()
+
+    def setup_saver(self):
+        self.saver = tf.train.Saver(tf.global_variables())
+
+    def restore_model(self,epoch = None):
+        if epoch is None:
+            self.saver.restore(self.sess,tf.train.latest_checkpoint(self.config.checkpoint_dir))
+        else:
+            self.saver.restore(self.sess,self.config.checkpoint_dir+ "model.ckpt" + ("-%d"%epoch))
+        print("restored model")
+    
+    def save_model(self,epoch=None):
+        if epoch is None:
+            self.saver.save(self.sess,self.config.checkpoint_dir + "model.ckpt",global_step=self.global_step)
+        else:
+            self.saver.save(self.sess,self.config.checkpoint_dir + "model.ckpt",global_step=epoch)
+        
+    def setup_input_placeholders(self):
+        # if we use beam search decoder, we need to specify the batch size and max source len
+        # this is a bug I think, and tf will fix this in the future
+        #if self.config.mode == "inference":
+        #    self.batch_size = self.config.batch_size
+        #    self.source_tokens = tf.placeholder(
+        #        tf.int32, shape=[self.batch_size, self.config.max_source_len])
+        #    self.source_length = tf.placeholder(
+        #        tf.int32, shape=[self.batch_size, ])
+        #else:
+        self.source_tokens = tf.placeholder(tf.int32, shape=[None, None])
+        self.source_length = tf.placeholder(tf.int32, shape=[None, ])
+        # using dynamic batch size
+        self.batch_size = tf.shape(self.source_tokens)[0]
+
+        if self.train_phase:
+            # the train data should pad with eos
+            self.target_tokens = tf.placeholder(tf.int32, shape=[None, None])
+            self.target_length = tf.placeholder(tf.int32, shape=[None, ])
+            
+            decoder_start_token = tf.ones(
+                shape=[self.batch_size, 1], dtype=tf.int32) * self.config.start_token
+            decoder_end_token = tf.ones(
+                shape=[self.batch_size, 1], dtype=tf.int32) * self.config.end_token  
+                
+            # decoder_inputs: [batch_size , max_time_steps + 1]
+            # insert _GO symbol in front of each decoder input
+            self.decoder_inputs = tf.concat([decoder_start_token,
+                                                  self.target_tokens], axis=1)
+                                                  
+            # decoder_inputs_length: [batch_size]
+            self.decoder_inputs_length = self.target_length + 1
+            
+            # decoder_targets: [batch_size, max_time_steps + 1]
+            # insert EOS symbol at the end of each decoder input
+            self.decoder_targets = tf.concat([self.target_tokens,
+                                                   decoder_end_token], axis=1)
 
     def setup_embedding(self):
         with tf.variable_scope("Embedding"):
@@ -102,12 +126,12 @@ class BasicS2SModel(object):
             self.encode_inputs = tf.nn.embedding_lookup(
                 self.encode_embedding, self.source_tokens)
             if self.train_phase:
-                self.decode_inputs = tf.nn.embedding_lookup(
-                    self.decode_embedding, self.target_tokens)
+                self.decoder_inputs = tf.nn.embedding_lookup(
+                    self.decode_embedding, self.decoder_inputs)
 
     def setup_multilayer_encoder(self):
         with tf.variable_scope("Encoder"):
-            encode_cell = multi_rnn_cell(self.config.encode_cell_type, self.config.encode_cell_size,
+            encode_cell = multi_rnn_cell(self.config.encode_cell_type, self.config.num_units,
                                          self.config.encode_layer_num, self.train_phase,
                                          self.config.keep_prob)
             outputs, states = tf.nn.dynamic_rnn(
@@ -118,9 +142,9 @@ class BasicS2SModel(object):
 
     def setup_bidirection_encoder(self):
         with tf.variable_scope("Encoder"):
-            fw_cell = single_rnn_cell(self.config.encode_cell_type, self.config.encode_cell_size,
+            fw_cell = single_rnn_cell(self.config.encode_cell_type, self.config.num_units,
                                       train_phase=self.train_phase, keep_prob=self.config.keep_prob)
-            bw_cell = single_rnn_cell(self.config.encode_cell_type, self.config.encode_cell_size,
+            bw_cell = single_rnn_cell(self.config.encode_cell_type, self.config.num_units,
                                       train_phase=self.train_phase, keep_prob=self.config.keep_prob)
 
             outputs, states = tf.nn.bidirectional_dynamic_rnn(
@@ -137,14 +161,14 @@ class BasicS2SModel(object):
         # use Dense layer to convert bi-direction state to decoder inital state
         with tf.variable_scope("Bi_Encode_State_Convert"):
             convert_layer = Dense(
-                self.config.decode_cell_size, dtype=tf.float32, name="bi_convert")
+                self.config.num_units, dtype=tf.float32, name="bi_convert")
             self.decode_initial_state = convert_layer(
                 tf.concat(self.encode_state, axis=1))
 
     def setup_attention_decoder(self):
         with tf.variable_scope("Decoder"):
             # multi-layer decoder
-            decode_cell = [single_rnn_cell(self.config.decode_cell_type, self.config.decode_cell_size,
+            decode_cell = [single_rnn_cell(self.config.decode_cell_type, self.config.num_units,
                                            self.train_phase, self.config.keep_prob) for i in range(self.config.decode_layer_num)]
 
             memory = self.encode_output
@@ -156,16 +180,12 @@ class BasicS2SModel(object):
                 memory_length = tf.contrib.seq2seq.tile_batch(
                     memory_length, self.config.beam_size)
 
-            atten_mech = tf.contrib.seq2seq.BahdanauAttention(
-                num_units=self.config.attention_size,
-                memory=memory,
-                memory_sequence_length=memory_length
-            )
+            atten_mech = create_attention_mechanism(self.config.attention_option,self.config.num_units,memory,memory_length)
 
             decode_cell[0] = tf.contrib.seq2seq.AttentionWrapper(
                 cell=decode_cell[0],
                 attention_mechanism=atten_mech,
-                attention_layer_size=self.config.attention_size
+                attention_layer_size=self.config.num_units
             )
 
             batch_size = self.batch_size
@@ -174,7 +194,6 @@ class BasicS2SModel(object):
                 batch_size = self.batch_size * self.config.beam_size
                 self.decode_initial_state = tf.contrib.seq2seq.tile_batch(
                     self.decode_initial_state, self.config.beam_size)
-                print(self.decode_initial_state)
             initial_state = [self.decode_initial_state for i in range(
                 self.config.decode_layer_num)]
             # initial state for attention cell
@@ -188,8 +207,7 @@ class BasicS2SModel(object):
                 self.config.vocab_size, kernel_initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1))
 
     def setup_beam_search(self):
-        start_tokens = tf.tile(tf.constant(
-            [self.config.start_token], dtype=tf.int32), [self.batch_size])
+        start_tokens = tf.fill([self.batch_size], self.config.start_token)
         bsd = tf.contrib.seq2seq.BeamSearchDecoder(
             cell=self.decode_cell,
             embedding=self.decode_embedding,
@@ -204,16 +222,18 @@ class BasicS2SModel(object):
             bsd,
             output_time_major=False,
             # impute_finished=True,
-            maximum_iterations=self.config.max_inference_lenght
+            maximum_iterations=self.config.max_inference_length
         )
         beam_predictions = final_outputs.predicted_ids
+        # TODO: found will got -1, beam search will add -1 when meet end token? and tf will delete start tokens?
+        # I will spend some time to read the source code of tf.contrib.seq2seq and tf.contrib.rnn
         self.beam_predictions = tf.transpose(beam_predictions, perm=[0, 2, 1])
 
     def setup_training_decode_layer(self):
-        max_dec_len = tf.reduce_max(self.target_length, name="max_dec_len")
+        max_dec_len = tf.reduce_max(self.decoder_inputs_length, name="max_dec_len")
         training_helper = tf.contrib.seq2seq.TrainingHelper(
-            self.decode_inputs,
-            self.target_length
+            self.decoder_inputs,
+            self.decoder_inputs_length
         )
         training_decoder = tf.contrib.seq2seq.BasicDecoder(
             cell=self.decode_cell,
@@ -231,14 +251,15 @@ class BasicS2SModel(object):
         # logits: [batch_size x max_dec_len x vocab_size]
         logits = tf.identity(train_dec_outputs.rnn_output, name='logits')
 
-        # targets: [batch_size x max_dec_len x vocab_size]
-        targets = tf.slice(self.target_tokens, [0, 0], [-1, max_dec_len], 'targets')
-
         masks = tf.sequence_mask(
-            self.target_length, max_dec_len, dtype=tf.float32, name="mask")
+            self.decoder_inputs_length, max_dec_len, dtype=tf.float32, name="mask")
+
+        # targets: [batch_size x max_dec_len]
+        # this is important, because we may have padded endings
+        targets = tf.slice(self.decoder_targets, [0, 0], [-1, max_dec_len], 'targets')
 
         self.losses = tf.contrib.seq2seq.sequence_loss(
-            logits=logits, targets=targets, weights=masks, name="losses")
+            logits=logits, targets=targets, weights=masks, name="losses",average_across_timesteps=True,average_across_batch=True,)
 
         # prediction sample for validation
         self.valid_predictions = tf.identity(
--- model/config.py
+++ model/config.py
@@ -12,33 +12,32 @@ class BasicConfig(object):
     decay_steps = 10000
 
     max_gradient_norm = 5.0
-    keep_prob = 0.75
+    keep_prob = 0.80
 
     vocab_size = 1000
     embedding_size = 256
-
+    
+    # dim size for encoder, decoder and attention
+    num_units = 128
     encode_cell_type = 'gru'
-    encode_cell_size = 256
     use_bidirection = False
     encode_layer_num = 2
 
-    attention_size = 10
-
     decode_cell_type = 'gru'
-    decode_cell_size = 256
     decode_layer_num = 2
+
+    attention_option = "bahdanau" # Bahdanau or Luong
 
     # These settings are used for beam search
     # beam size = 1 will use greedy search
     beam_size = 5
     # only useful for beam search
     batch_size = 32
-    max_source_len = 10
-    max_inference_lenght = 10
+    max_inference_length = 10
 
     checkpoint_dir = "/tmp/basic_s2s/"
 
     def __repr__(self):
-        arttribute = vars(BasicConfig)
+        arttribute = vars(self)
         arttribute = {k:v for k,v in arttribute.items() if not k.startswith("__")}
         return str(arttribute)
